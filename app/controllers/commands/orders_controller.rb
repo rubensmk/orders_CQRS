500657d85ae4bc37ffbd7ddf2ca8af701394feaa
Rubens Mititaka Kishimoto <rubenskishimoto@gmail.com>
2024-06-19 22:56:24 -0300

    fix: remove comments

--- app/controllers/commands/orders_controller.rb
+++ app/controllers/commands/orders_controller.rb
@@ -3,12 +3,9 @@ module Commands
     skip_before_action :verify_authenticity_token, only: [:create]
 
     def create
-      # Criar uma nova order no MySQL (ActiveRecord)
       mysql_order = Order.new(order_params)
 
-      # Salvar no MySQL
       if mysql_order.save
-        # Criar uma nova order no MongoDB (Mongoid)
         mongo_order = ReadOnly::Order.new(order_params)
         mongo_order.save
 
